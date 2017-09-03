787a43bc22acbf20291f225f9a3d3313b3a0b826
DataandGoliath <Njernigan4@protonmail.com>
2017-09-03 13:51:34 -0400

    Add files via upload

---
+++ IPTrack.py
@@ -0,0 +1,21 @@
+import os
+os.system("wget -N -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz")
+os.system("gzip -d GeoLiteCity.dat.gz")
+print("Database downloaded")
+print("Installing Dependancies")
+os.system("wget http://pygeoip.googlecode.com/files/pygeoip-0.1.3.zip")
+os.system("pip install pygeoip")
+os.system("wget http://svn.python.org/projects/sandbox/trunk/setuptools/ez_setup.py")
+os.system("wget http://pypi.python.org/packages/2.5/s/setuptools-0.6c11-py2.5.egg")
+os.system("pip install setuptools")
+import pygeoip
+gip = pygeoip.GeoIP('GeoLiteCity.dat')
+print("Setup complete. Ready to query.")
+raw_input("Please press enter to begin.")
+os.system("clear")
+while True:
+	IP = str(raw_input("IP address: "))
+	rec = gip.record_by_addr(IP)
+	for key,val in rec.items():
+		print(str(key)+": "+str(val))
+	print("\n")
